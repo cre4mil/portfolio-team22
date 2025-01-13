7fd4e89d3e149acce75b2cb3428356728de243ef
cre4mil <panisarakunkam@gmail.com>
2025-01-13 18:13:18 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      // ส่วนขยายธีมเพิ่มเติมจะอยู่ที่นี่
+    },
+  },
+  plugins: [],
+};
